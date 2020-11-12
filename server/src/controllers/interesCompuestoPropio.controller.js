912a0951571bc5f0d25a206dc82ec4dff557bc51
alfredoperaltag <alfredo08peralta@gmail.com>
2020-11-12 04:22:50 -0600

    add generateGanancia interesCompuestoPropio models, server

--- server/src/controllers/interesCompuestoPropio.controller.js
+++ server/src/controllers/interesCompuestoPropio.controller.js
@@ -8,13 +8,9 @@ interesCompuestoPropioCtrl.getInteresCompuestoPropios = async (req, res, next) =
 }
 
 interesCompuestoPropioCtrl.postInteresCompuestoPropios = async (req, res, next) => {
-    const gananciaHistorica = (req.body.interesesCompuestos - req.body.ingresosExtrasMensuales).toFixed(2)
-    const ultimateInteresCompuestoPropio = await InteresCompuestoPropio.find().sort({ $natural: -1 }).limit(1)
-    const ganancia = (gananciaHistorica - ultimateInteresCompuestoPropio[0].gananciaHistorica).toFixed(2)
-    let porcentaje = 0
-    if (req.body.interesesCompuestos !== 0) {
-        porcentaje = (((ganancia / 30) / req.body.interesesCompuestos) * 36500).toFixed(2)
-    }
+    const generateGanancia = await InteresCompuestoPropio.generateGanancia(req.body._id, req.body.interesesCompuestos, req.body.ingresosExtrasMensuales)
+    const { ganancia, gananciaHistorica, porcentaje } = generateGanancia
+
     const interesCompuestoPropio = new InteresCompuestoPropio({
         meses: req.body.meses,
         ingresosExtrasMensuales: req.body.ingresosExtrasMensuales,
@@ -28,20 +24,9 @@ interesCompuestoPropioCtrl.postInteresCompuestoPropios = async (req, res, next) 
 }
 
 interesCompuestoPropioCtrl.putInteresCompuestoPropios = async (req, res, next) => {
-    const gananciaHistorica = (req.body.interesesCompuestos - req.body.ingresosExtrasMensuales).toFixed(2)
-    const interesesCompuestosPropios = await InteresCompuestoPropio.find()
-    const positionInteresCompuestoPropio = interesesCompuestosPropios.findIndex(element => element.id === req.body._id)
-    const beforeInteresCompuestoPropio = interesesCompuestosPropios[positionInteresCompuestoPropio - 1]
-    let ganancia = 0
-    if (beforeInteresCompuestoPropio) {
-        if (beforeInteresCompuestoPropio.gananciaHistorica !== undefined) {
-            ganancia = (gananciaHistorica - beforeInteresCompuestoPropio.gananciaHistorica).toFixed(2)
-        }
-    }
-    let porcentaje = 0
-    if (req.body.interesesCompuestos !== 0) {
-        porcentaje = (((ganancia / 30) / req.body.interesesCompuestos) * 36500).toFixed(2)
-    }
+    const generateGanancia = await InteresCompuestoPropio.generateGanancia(req.body._id, req.body.interesesCompuestos, req.body.ingresosExtrasMensuales)
+    const { ganancia, gananciaHistorica, porcentaje } = generateGanancia
+
     req.body = { ...req.body, ganancia, porcentaje, gananciaHistorica }
     const { id } = req.params
     const interesCompuestoPropio = await InteresCompuestoPropio.findByIdAndUpdate(id, { $set: req.body }, { new: true })
--- server/src/models/interesCompuestoPropio.js
+++ server/src/models/interesCompuestoPropio.js
@@ -13,4 +13,32 @@ const interesCompuestoPropio = new Schema({
     versionKey: false
 })
 
-module.exports = mongoose.model('InteresCompuestoPropio', interesCompuestoPropio)
+interesCompuestoPropio.statics.generateGanancia = async (id, interesesCompuestos, ingresosExtrasMensuales) => {
+    const gananciaHistorica = (interesesCompuestos - ingresosExtrasMensuales).toFixed(2)
+
+    let ganancia = 0
+    if (id) {
+        const interesesCompuestosPropios = await InteresCompuestoPropio.find()
+        const positionInteresCompuestoPropio = interesesCompuestosPropios.findIndex(element => element.id === id)
+        const beforeInteresCompuestoPropio = interesesCompuestosPropios[positionInteresCompuestoPropio - 1]
+        if (beforeInteresCompuestoPropio) {
+            if (beforeInteresCompuestoPropio.gananciaHistorica !== undefined) {
+                ganancia = (gananciaHistorica - beforeInteresCompuestoPropio.gananciaHistorica).toFixed(2)
+            }
+        }
+    } else {
+        const ultimateInteresCompuestoPropio = await InteresCompuestoPropio.find().sort({ $natural: -1 }).limit(1)
+        ganancia = (gananciaHistorica - ultimateInteresCompuestoPropio[0].gananciaHistorica).toFixed(2)
+    }
+
+    let porcentaje = 0
+    if (interesesCompuestos !== 0) {
+        porcentaje = (((ganancia / 30) / interesesCompuestos) * 36500).toFixed(2)
+    }
+
+    return { gananciaHistorica, ganancia, porcentaje }
+}
+
+const InteresCompuestoPropio = mongoose.model('InteresCompuestoPropio', interesCompuestoPropio)
+
+module.exports = InteresCompuestoPropio
